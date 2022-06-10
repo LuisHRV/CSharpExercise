665f6e2dfd33d1c7c92ec61ba720d36518844875
LuisHRV <luis.h01@hotmail.com>
2022-06-10 17:49:49 -0300

    Add: New FOR exercises

---
+++ Exercicios-For/Exercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio3 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Qauntos numeros voce vai colocar?");
+            int numb = int.Parse(Console.ReadLine());
+            int inValue = 0, outValue = 0, addValue;
+
+            for (int i = 0; i < numb; i++) {
+
+                Console.Write("Adicione o valor: ");
+                addValue = int.Parse(Console.ReadLine());
+
+                if (addValue >= 10 && addValue <= 20) {
+
+                    inValue++;
+
+                } else {
+
+                    outValue++;
+
+                }
+            }
+            Console.WriteLine($"{inValue} in \n{outValue} out");
+        }
+    }
+}
---
+++ Exercicios-For/Exercicios-For/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio1 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Quantos Numero voce vai digitar?");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            int time = 0;
+
+            for (int i = 0; i < n; i++) {
+
+                time++;
+
+                Console.Write($"Valor #{time}:");
+                int value = int.Parse(Console.ReadLine());
+
+                soma = soma + value;
+            }
+
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
