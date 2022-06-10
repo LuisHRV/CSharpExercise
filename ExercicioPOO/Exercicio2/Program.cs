6156bf279762542cdc06b0a038f90ba11a83e881
LuisHRV <luis.h01@hotmail.com>
2022-06-10 18:43:38 -0300

    Add: New POO excises

---
+++ ExercicioPOO/Exercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio2 {
+    class Program {
+        public static void Main(string[] args) {
+
+            Person personA = new Person();
+            Person personB = new Person();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            personA.Name = Console.ReadLine();
+            Console.Write("Idade: ");
+            personA.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            personB.Name = Console.ReadLine();
+            Console.Write("Idade: ");
+            personB.Age = int.Parse(Console.ReadLine());
+
+            if (personA.Age > personB.Age) {
+                Console.WriteLine($"Pessoa mais velha: {personA.Name}");
+            }else {
+                Console.WriteLine($"Pessoa mais velha: {personB.Name}");
+            }
+        }
+    }
+}
---
+++ ExercicioPOO/ExercicioPOO/Triangulo.cs
@@ -0,0 +1,9 @@
+﻿namespace ExerciciosPOO {
+    class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
