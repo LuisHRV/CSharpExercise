efd8b617a455e62e0b1df88fc506d1d8968d286b
LuisHRV <luis.h01@hotmail.com>
2022-07-07 13:44:54 -0300

    Add: New exercises

---
+++ ExerciciosSET/ExerciciosSET/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ExerciciosSET.Entities;
+
+namespace ExerciciosSET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecords> set = new HashSet<LogRecords>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecords { UserName = name, Instant = instant });
+                    }
+                    Console.WriteLine($"Total Users: {set.Count}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
