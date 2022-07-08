67926f34f7b42c2122528210d4bca1457f60592f
LuisHRV <luis.h01@hotmail.com>
2022-07-08 01:38:05 -0300

    Add: Exercise Api

---
+++ Api/ToDo/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDo.Data;
+using ToDo.Models;
+
+namespace ToDo.Controllers {
+    [ApiController]
+    public class HomeController : ControllerBase{
+        [HttpGet("/")]
+        public IActionResult Get([FromServices]AppDbContext context) => Ok(context.ToDos.ToList());
+
+        [HttpGet("/{id:int}")]
+        public IActionResult GetById(
+            [FromRoute]int id,
+            [FromServices]AppDbContext context) {
+            
+            var todo = context.ToDos.FirstOrDefault(x => x.Id == id);
+            if (todo == null){
+                return NotFound();
+            }
+            return Ok(todo);
+        }
+
+        [HttpPost("/")]
+        public IActionResult Post(
+            [FromBody]ToDoModels todo,
+            [FromServices]AppDbContext context) {
+            context.ToDos.Add(todo);
+            context.SaveChanges();
+            return Created($"{todo.Id}", todo);
+        }
+        [HttpPut("/{id:int}")]
+        public IActionResult Put(
+            [FromRoute] int id,
+            [FromBody]ToDoModels todo,
+            [FromServices]AppDbContext context) {
+            var model = context.ToDos.FirstOrDefault(x => x.Id == id);
+            if(model == null){
+                return NotFound();
+            }
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+            
+            context.ToDos.Update(model);
+            context.SaveChanges();
+            return Ok(model);
+        }
+        [HttpDelete("/{id:int}")]
+        public IActionResult Delete(
+            [FromRoute] int id,
+            [FromServices]AppDbContext context
+        ) {
+            var model = context.ToDos.FirstOrDefault(x => x.Id == id);
+            if (model == null){
+                return NotFound();
+            }
+            context.ToDos.Remove(model);
+            context.SaveChanges();
+            return Ok(model);
+        }
+    }
+}
