4f99ceb5ba1440da7b6a4788109835c326b3ff2f
LuisHRV <luis.h01@hotmail.com>
2022-07-18 04:38:29 -0300

    Add: New Exercises

---
+++ ExtensionMetods/ExtensionMetods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ExtensionMetods.Extensions
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisobj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisobj);
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture)+" hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture)+ " days";
+            }
+        }
+    }
+}
---
+++ ExtensionMetods/ExtensionMetods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExtensionMetods.Extensions;
+
+namespace ExtensionMetods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 7,8,11,20,20);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
